9810c6707a86f85bee02dfebe3d1773c5b5c07f5
YevhenKhariuk <777genya2009@gmail.com>
2023-05-23 21:42:14 +0300

    виправлення - додав trim()

--- js/task-05.js
+++ js/task-05.js
@@ -6,5 +6,5 @@ const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
 nameInput.addEventListener("input", () => {
-  nameOutput.textContent = nameInput.value || "Anonymous";
+  nameOutput.textContent = nameInput.value.trim() || "Anonymous";
 });
